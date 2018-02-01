09c23be4954d17c1a793bfbb0cbe7e1bd9369c42
Forever <lynk0613@hotmail.com>
2018-01-31 23:24:12 -0500

    main structure

--- vueapp01/src/components/DrawingSvg.vue
+++ vueapp01/src/components/DrawingSvg.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <svg>
+      <defs>
+        <pattern id="pattern1" patternUnits="userSpaceOnUse" width="150" height="150">
+          <image xlink:href="../assets/pattern1.jpeg" x="0" y="0"/>
+        </pattern>
+        <pattern id="pattern2" patternUnits="userSpaceOnUse" width="150" height="150">
+          <image xlink:href="../assets/pattern2.jpeg" x="0" y="0"/>
+        </pattern>
+        <pattern id="pattern3" patternUnits="userSpaceOnUse" width="150" height="150">
+          <image xlink:href="../assets/pattern3.jpeg" x="0" y="0"/>
+        </pattern>
+        <mask id="part1">
+          <polygon  x="10" points="270,0  0,500 600,500" fill="#ffffff"/>
+        </mask>
+      </defs>
+    </svg>
+    <svg viewBox="0 0 800 800">
+      <polygon class="pattern" x="10" points="270,0  0,500 600,500" fill="url(#pattern3)"/>
+      <image class="part" xlink:href="../assets/pattern1.jpeg" width="800" height="800" style="mask:url(#part1)"/>
+    </svg>
   </div>
 </template>
 
@@ -25,18 +45,20 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .main-canvas {
+    width: 345px;
+    height: 500px;
+    position: absolute;
+    right: 0;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    margin: auto;
+  }
+  .part{
+    mix-blend-mode: multiply;
+  }
+  .pattern{
+    mix-blend-mode: multiply;
+  }
 </style>
